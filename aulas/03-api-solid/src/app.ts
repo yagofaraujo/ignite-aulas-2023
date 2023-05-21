270662e66b0042d84005d2e3205e6f8c7644fc68
Yago Araújo <yagoferreira556@gmail.com>
2023-05-21 00:07:56 -0300

    chore: primeiro caso de uso e design patterns

--- aulas/03-api-solid/src/app.ts
+++ aulas/03-api-solid/src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  } else {
+    // LOG PARA UMA FERRAMENTA EXTERNA
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
