7990bfb65b762f2bee713076bfc3715c1d146c96
Yago Araújo <yagoferreira556@gmail.com>
2023-05-31 20:44:48 -0300

    chore: ajuste na estrutura de pastas de acordo com arquitetura limpa

--- nodejs/aulas/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
+++ nodejs/aulas/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswersRepository } from '../repositories/answers-repository'
-import { Answer } from '../entities/answer'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
   create: async (answer: Answer) => {},
--- nodejs/aulas/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.ts
+++ nodejs/aulas/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.ts
@@ -1,5 +1,5 @@
-import { UniqueEntityId } from '../../core/entities/value-objects/unique-entity-id'
-import { Answer } from '../entities/answer'
+import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
