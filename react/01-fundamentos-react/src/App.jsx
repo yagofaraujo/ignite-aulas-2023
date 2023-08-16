365e00ad0679caae3943c3148a1330b19e4cc1f2
Yago Araújo <yagoferreira556@gmail.com>
2023-08-16 20:15:53 -0300

    feat: add post component

--- react/01-fundamentos-react/src/App.jsx
+++ react/01-fundamentos-react/src/App.jsx
@@ -1,8 +1,9 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
 import styles from './App.module.css'
 import './global.css'
-import { Post } from './Post'
 
 function App() {
   return (
--- react/01-fundamentos-react/src/Post.jsx
+++
@@ -1,8 +0,0 @@
-export function Post(props) {
-  return (
-    <div>
-      <strong>{props.author}</strong>
-      <p id="">{props.content}</p>
-    </div>
-  )
-}
