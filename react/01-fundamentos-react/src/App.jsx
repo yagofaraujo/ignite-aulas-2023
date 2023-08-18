28a1ad68252bf9b04b85a2b50610c4bf3812bcd0
Yago MacOS <yagoferreira556@gmail.com>
2023-08-18 17:56:23 -0300

    chore: add posts array to generate dynamic posts

--- react/01-fundamentos-react/src/App.jsx
+++ react/01-fundamentos-react/src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -13,14 +44,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat ea neque dolor reiciendis, porro quos, nostrum cumque rem consequuntur sint commodi rerum tempore corrupti, a labore alias voluptatibus! Sapiente, eius!"
-          />      
-          <Post 
-            author="Yago Ferreira"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat ea neque dolor reiciendis, porro quos, nostrum cumque rem consequuntur sint commodi rerum tempore corrupti, a labore alias voluptatibus! Sapiente, eius!"
-          />     
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}            
+              />
+            )
+          })}
         </main>
       </div>
     </div>
