47043a3b1309cf3817076e5c03965a0a52949cbc
Yago MacOS <yagoferreira556@gmail.com>
2023-08-18 18:27:23 -0300

    chore: add dynamic post properties

--- react/01-fundamentos-react/src/components/Post.jsx
+++ react/01-fundamentos-react/src/components/Post.jsx
@@ -1,30 +1,47 @@
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 import styles from './Post.module.css'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Post() {
+export function Post({ author, content, publishedAt}) {
+  const formattedPublishedDate = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
-            src="https://github.com/yagofaraujo.png" />
+            src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Yago Ferreira</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='11 de maio às 08:13h' dateTime='2022-05-11 08:13:30'>
-          Publicado há 1h
+        <time
+          title={formattedPublishedDate}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#rocketseat</a></p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
