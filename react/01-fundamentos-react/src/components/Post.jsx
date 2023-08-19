04739d6d785a18e695f6e744a8823e4a7122cd98
Yago MacOS <yagoferreira556@gmail.com>
2023-08-19 13:08:34 -0300

    fix: fixes console erros due components empty key property

--- react/01-fundamentos-react/src/components/Post.jsx
+++ react/01-fundamentos-react/src/components/Post.jsx
@@ -24,13 +24,13 @@ export function Post({ author, content, publishedAt}) {
   function handleCreateNewComment() {
     event.preventDefault()
 
-    const newCommentText = event.target.comment.value
     setComments([...comments, newCommentText])
 
     setNewCommentText('')
   }
 
   function handleNewCommentChange() {
+    event.preventDefault()
     setNewCommentText(event.target.value)
   }
 
@@ -57,9 +57,9 @@ export function Post({ author, content, publishedAt}) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -81,7 +81,7 @@ export function Post({ author, content, publishedAt}) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment}/>
+          return <Comment key={comment} content={comment}/>
         })}
       </div>
     </article>
