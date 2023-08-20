ba66869433bc712accccdb1c613515fd321e0d17
Yago MacOS <yagoferreira556@gmail.com>
2023-08-20 10:43:10 -0300

    chore: add and config project structure

--- react/02-ignite-timer/src/App.tsx
+++ react/02-ignite-timer/src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { Button } from './components/Button'
+
 export function App() {
   return (
-    <h1>Hello world!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
