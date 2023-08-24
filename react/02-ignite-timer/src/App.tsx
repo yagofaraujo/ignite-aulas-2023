2e653daa2592d11b0a852c48148590795cca586c
Yago Araújo <yagoferreira556@gmail.com>
2023-08-24 19:10:15 -0300

    feat: add default layout component and home & history pages

--- react/02-ignite-timer/src/App.tsx
+++ react/02-ignite-timer/src/App.tsx
@@ -1,15 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
-import { Button } from './components/Button';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
