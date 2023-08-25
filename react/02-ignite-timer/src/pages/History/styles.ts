42388d602a0eae42a40be23771ddd453433ad13b
Yago MacOS <yagoferreira556@gmail.com>
2023-08-25 16:50:01 -0300

    feat: add status component to projects history list

--- react/02-ignite-timer/src/pages/History/styles.ts
+++ react/02-ignite-timer/src/pages/History/styles.ts
@@ -54,3 +54,29 @@ export const HistoryList = styled.div`
     }
   }
 `;
+
+const STATUS_COLORS = {
+  inProgress: 'yellow-500',
+  completed: 'green-500',
+  stoped: 'red-500',
+} as const;
+
+interface StatusProps {
+  statuscolor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span.withConfig({
+  shouldForwardProp: (prop) => prop === 'statuscolor' || prop === 'children',
+})<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statuscolor]]};
+  }
+`;
