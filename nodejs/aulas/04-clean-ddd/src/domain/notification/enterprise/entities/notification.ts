8a5fcccbc5b803f9bb13f8ddc10b2902792a2b8e
Yago Araújo <yagoferreira556@gmail.com>
2023-09-27 12:12:11 -0300

    feat: add notification entity

---
+++ nodejs/aulas/04-clean-ddd/src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,45 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface NotificationProps {
+  recipientId: UniqueEntityId
+  title: string
+  content: string
+  createdAt: Date
+  readAt?: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get readAt() {
+    return this.props.readAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const notification = new Notification({
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    })
+
+    return notification
+  }
+}
